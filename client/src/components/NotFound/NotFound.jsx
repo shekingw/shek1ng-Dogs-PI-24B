6ab3e116e9a07bbbc7d8e17af1cf4ed3cd339ebd
shek1ng <sheking142@gmail.com>
2022-05-24 13:57:30 -0300

    finished

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,13 +1,30 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './NotFound.css';
+import image404 from '../../assets/404.png';
+import Nav from '../NavBar/NavBar';
+import { useEffect } from 'react';
+
+const NotFound404 = () => {
+	const navigate = useNavigate();
+
+	function timer() {
+		navigate('/home');
+	}
+	useEffect(() => {
+		setTimeout(timer, 5000);
+	}, []);
+};
 
 export default function NotFound() {
 	return (
-		<div className="container-notFound">
-			<Link to="/home">
-				<button>Regresar</button>
-			</Link>
-			<h1>Error 404 NOT FOUND</h1>;
+		<div className="container404">
+			<div className="container-notFound">
+				<Nav />
+				<img src={image404} className="image404" />
+				<p>Seras redireccionado en 5 segundos..</p>
+				{NotFound404()}
+			</div>
 		</div>
 	);
 }
